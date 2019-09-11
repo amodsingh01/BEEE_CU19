da15344bbf7b29303b784f2c2acec94a6245c024
amodsingh01 <54638981+amodsingh01@users.noreply.github.com>
2019-09-11 21:42:04 +0530

    Create code.ino

---
+++ Exp_2_LED_chaser/code.ino
@@ -0,0 +1,14 @@
+void setup()
+{pinMode(1,OUTPUT);
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT):
+}
+
+void loop()
+{for(int i=1;int<=4;i++)
+digitalWrite(i,HIGH);
+delay(600);
+digitalWrite(i,LOW);
+delay(600);
+}
